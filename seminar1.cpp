55d6df54efac52ca6fc71e7659e1f3f1d8527308
IoanaMarica1 <ioanamarica66@gmail.com>
2025-03-06 19:57:47 +0200

    Dog class.

--- seminar1.cpp
+++ seminar1.cpp
@@ -1,6 +1,27 @@
-//
-// Created by Asus on 3/6/2025.
-//
+#include <iostream>
+
+class Dog {
+public:
+    bool eat() {
+        std::cout<<"Dog eats";
+        return true;
+    }
+    int getAge() {
+        return age;
+    }
+    int setAge(int varsta) {
+        age=varsta;
+    }
+private:
+    int age;
+};
 int main() {
+    Dog dog1=Dog();
+    dog1.eat();
+    dog1.setAge(5);
+    dog1.getAge();
+    std::cout<<dog1.getAge();
+
     return 0;
 }
+
