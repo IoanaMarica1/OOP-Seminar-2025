c3ba7b5eb7e24afc5822f07c12d0a7040736156f
Ioana Marica <ioanamarica66@gmail.com>
2025-03-20 18:37:36 +0200

    Add files via upload

---
+++ seminar2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 3/20/2025.
+//
+
+#include "seminar2.h"
---
+++ seminar2.h
@@ -0,0 +1,16 @@
+//
+// Created by Asus on 3/20/2025.
+//
+
+#ifndef SEMINAR2_H
+#define SEMINAR2_H
+
+
+
+class seminar2 {
+
+};
+
+
+
+#endif //SEMINAR2_H
